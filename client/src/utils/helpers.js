4ae5dd176056936da11bf06f7aa51bcdcf483ee3
JR728 <135184435+JR728@users.noreply.github.com>
2023-11-20 19:55:33 -0600

    added formDate and valid email

---
+++ client/src/utils/helpers.js
@@ -0,0 +1,11 @@
+// client/src/utils/helpers.js
+export const formatDate = (date) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(date).toLocaleDateString(undefined, options);
+};
+
+export const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+};
+  
