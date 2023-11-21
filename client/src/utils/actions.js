00b67db8420920a8de95c3408969343ed24f244c
kamilamontes <kdmontes21@gmail.com>
2023-11-21 17:27:10 -0600

    added action.js

---
+++ client/src/utils/actions.js
@@ -0,0 +1,10 @@
+export const ADD_EVENT = 'ADD_EVENT';
+
+export const REMOVE_EVENT = 'REMOVE_EVENT';
+
+export const SAVE_EVENT = 'SAVE_EVENT';
+
+export const ADD_TICKET = 'ADD_TICKET';
+
+export const REMOVE_TICKET = 'REMOVE_TICKET';
+
