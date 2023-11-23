2b8bc5c335833ade4eb45528d41bf4d8a3b5e4c9
Cdieh4 <you@example.com>
2023-11-22 21:10:18 -0600

    Reducers.js done

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,28 @@
+import {  Events_List  } from '.actions';
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        case 'EVENTS_LIST': {
+            const newID = createID(state.events);
+            const newEvent = {...action.payload, id: newID }
+
+            return {
+                ...state,
+                events: [...state.events, newEvent],
+            };
+        }
+    case 'REMOVE_EVENT': {
+        return {
+           ...state,
+           events: state.events.filter((events) => events.id !== action.payload),
+        };
+    }
+    default: {
+        return state;
+    }
+
+
+    
+
+    }
+}
