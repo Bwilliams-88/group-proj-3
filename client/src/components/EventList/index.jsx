28800c186a939ea13947f89a578b0982956692b8
Cdieh4 <you@example.com>
2023-11-28 15:09:47 -0600

    index.jsx

--- client/src/components/EventList/index.jsx
+++ client/src/components/EventList/index.jsx
@@ -1,22 +1,22 @@
 import {
-    ADD_EVENT,
-    REMOVE_EVENT,
-    ADD_TO_CART,
-    CLEAR_CART
+  ADD_EVENT,
+  REMOVE_EVENT,
+  ADD_TO_CART,
+  CLEAR_CART
 } from '../utils/actions';
 
-import {useGlobalState} from '../utils/GlobalState';
+import { useGlobalState } from '../utils/GlobalState';
 
 export default function EventList() {
-    const [state, dispatch]= useGlobalState;
+  const [state, dispatch] = useGlobalState;
 
-    return (
-        <div>
-            {state.events ? (
-              <>
-                <section eventName="event-list">
-                  <table>
-                    <thead>
+  return (
+    <div>
+      {state.events ? (
+          <>
+          <section eventName="event-list">
+            <table>
+              <thead>
                 <tr>
                   <th>ID</th>
                   <th>Name</th>
@@ -26,8 +26,8 @@ export default function EventList() {
               </thead>
 
               <tbody>
-               {/* Access events from our state object returned from useReducer */}
-               {state.events.map((events) => (
+                {/* Access events from our state object returned from useReducer */}
+                {state.events.map((events) => (
                   <tr key={events.id}>
                     <td>{events.id}</td>
                     <td>{events.name}</td>
@@ -46,29 +46,29 @@ export default function EventList() {
                       > 
            
         }
-        </select>
+                      </select>
 
-              <button
-                type="button"
-                onClick={() => {
-                  console.log('EventList.js: Dispatched add event! ');
-                  return dispatch({
-                    type: ADD_EVENT,
-                    payload: {
-                      name: state.eventName,
-                      category: state.eventCategory,
-                    },
-                  });
-                }}
-              >
-                Add <Event></Event>
-              </button>
-            </div>
-          </section>
+                      <button
+                        type="button"
+                        onClick={() => {
+                          console.log('EventList.js: Dispatched add event! ');
+                          return dispatch({
+                            type: ADD_EVENT,
+                            payload: {
+                              name: state.eventName,
+                              category: state.eventCategory,
+                            },
+                          });
+                        }}
+                      >
+                        Add <Event></Event>
+                      </button>
+                    </div>
+                  </section>
         </>
-        </div>
+            </div>
 
-        <div className="add-event">
+            <div className="add-event">
               <input
                 value={state.eventCategory}
                 onChange={(e) =>
@@ -76,13 +76,20 @@ export default function EventList() {
                 }
                 placeholder="New cart..."
                 type="text"
-    )
+              />
+                <select>
+                onChange={(e) =>
+                  dispatch({ type: CLEAR_CART, payload: e.target.value })
+                }
+                value={state.eventCategory}
+              >
+                <option>Remove Cart Items...</option>
+          
+    
 
-
+  
 
 }
-
-
 
 
 
